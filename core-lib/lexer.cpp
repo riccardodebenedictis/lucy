dde480d230c917da54c1008f655418a43273d77c
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-06-16 17:54:15 +0200

    minor lexer improvements..

--- core-lib/lexer.cpp
+++ core-lib/lexer.cpp
@@ -1,4 +1,7 @@
 #include "lexer.h"
+#include <vector>
+#include <iostream>
+#include <string>
 
 namespace lucy
 {
@@ -9,6 +12,109 @@ lexer::~lexer() {}
 
 token lexer::next()
 {
-    return {symbol::EOF_Symbol, 0, 0};
+    while (true)
+    {
+        ch = is.get();
+        switch (ch)
+        {
+        case '"': // string literal
+        {
+            std::vector<char> str;
+            end_pos++;
+            while (true)
+            {
+                ch = is.get();
+                switch (ch)
+                {
+                case '"':
+                    end_pos++;
+                    return mk_string_token(std::string(str.begin(), str.end()));
+                case '\\':
+                    // read escaped char
+                    end_pos++;
+                    ch = is.get();
+                    str.push_back(ch);
+                    break;
+                case '\r':
+                case '\n':
+                    error("newline in string literal..");
+                    return mk_token(symbol::ERROR);
+                case EOF:
+                    error("invalid string literal..");
+                    return mk_token(symbol::ERROR);
+                default:
+                    str.push_back(ch);
+                }
+            }
+        }
+        case '/':
+            end_pos++;
+            ch = is.get();
+            switch (ch)
+            {
+            case '/': // in single-line comment
+                end_pos++;
+                while (true)
+                {
+                    ch = is.get();
+                    end_pos++;
+                    switch (ch)
+                    {
+                    case '\r':
+                        if (is.get() != '\n')
+                        {
+                            is.unget();
+                        }
+                    case '\n':
+                        end_line++;
+                        end_pos = 0;
+                    case EOF:
+                        return mk_token(symbol::COMMENT);
+                    }
+                }
+            case '*': // in multi-line comment
+                end_pos++;
+                while (true)
+                {
+                    ch = is.get();
+                    end_pos++;
+                    switch (ch)
+                    {
+                    case '*':
+                        ch = is.get();
+                        end_pos++;
+                        if (ch == '/')
+                        {
+                            return mk_token(symbol::COMMENT);
+                        }
+                        break;
+                    case '\r':
+                        if (is.get() != '\n')
+                        {
+                            is.unget();
+                        }
+                    case '\n':
+                        end_line++;
+                        end_pos = 0;
+                    }
+                }
+            }
+            is.unget();
+        case '\r':
+            if (is.get() != '\n')
+            {
+                is.unget();
+            }
+        case '\n':
+            end_line++;
+            end_pos = 0;
+            break;
+        default:
+            error("invalid token..");
+            return mk_token(symbol::ERROR);
+        }
+    }
 }
+
+void lexer::error(const std::string &err) { std::cerr << "[" << std::to_string(start_line) << ", " << std::to_string(start_pos) << err << std::endl; }
 }
--- core-lib/lexer.h
+++ core-lib/lexer.h
@@ -53,14 +53,53 @@ enum symbol
   ID,             // ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*
   NumericLiteral, // [0-9]+ ('.' [0-9]+)? | '.' [0-9]+
   StringLiteral,  // '" . . ."'
+  COMMENT,
+  ERROR,
   EOF_Symbol
 };
 
-struct token
+class token
 {
+public:
+  token(const symbol &sym, const int &start_line, const int &start_pos, const int &end_line, const int &end_pos) : sym(sym), start_line(start_line), start_pos(start_pos), end_line(end_line), end_pos(end_pos) {}
+  virtual ~token() {}
+
+private:
   symbol sym;
-  int pos;
-  int line;
+  int start_line;
+  int start_pos;
+  int end_line;
+  int end_pos;
+};
+
+class id_token : public token
+{
+public:
+  id_token(const symbol &sym, const int &start_line, const int &start_pos, const int &end_line, const int &end_pos, const std::string &id) : token(sym, start_line, start_pos, end_line, end_pos), id(id) {}
+  virtual ~id_token() {}
+
+private:
+  std::string id;
+};
+
+class numeric_token : public token
+{
+public:
+  numeric_token(const symbol &sym, const int &start_line, const int &start_pos, const int &end_line, const int &end_pos, const double &val) : token(sym, start_line, start_pos, end_line, end_pos), val(val) {}
+  virtual ~numeric_token() {}
+
+private:
+  double val;
+};
+
+class string_token : public token
+{
+public:
+  string_token(const symbol &sym, const int &start_line, const int &start_pos, const int &end_line, const int &end_pos, const std::string &str) : token(sym, start_line, start_pos, end_line, end_pos), str(str) {}
+  virtual ~string_token() {}
+
+private:
+  std::string str;
 };
 
 class lexer
@@ -73,6 +112,46 @@ public:
   token next();
 
 private:
+  token mk_token(const symbol &sym)
+  {
+    token tk(sym, start_line, start_pos, end_line, end_pos);
+    start_line = end_line;
+    start_pos = end_pos;
+    return tk;
+  }
+
+  id_token mk_id_token(const std::string &id)
+  {
+    id_token tk(symbol::ID, start_line, start_pos, end_line, end_pos, id);
+    start_line = end_line;
+    start_pos = end_pos;
+    return tk;
+  }
+
+  numeric_token mk_numeric_token(const double &val)
+  {
+    numeric_token tk(symbol::NumericLiteral, start_line, start_pos, end_line, end_pos, val);
+    start_line = end_line;
+    start_pos = end_pos;
+    return tk;
+  }
+
+  string_token mk_string_token(const std::string &str)
+  {
+    string_token tk(symbol::StringLiteral, start_line, start_pos, end_line, end_pos, str);
+    start_line = end_line;
+    start_pos = end_pos;
+    return tk;
+  }
+
+  void error(const std::string &err);
+
+private:
   std::istream &is;
+  char ch;
+  int start_line = 0;
+  int start_pos = 0;
+  int end_line = 0;
+  int end_pos = 0;
 };
 }
