4a98c054f244aa09cd61981fd6d2d4833a3e3b7e
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-06-16 14:23:40 +0200

    trying custom parser..

--- core-lib/parser/ratioListener.cpp
+++
@@ -1,6 +0,0 @@
-
-// Generated from ratio.g4 by ANTLR 4.7
-
-#include "ratioListener.h"
-
-using namespace lucy;
--- core-lib/parser/ratioVisitor.cpp
+++
@@ -1,6 +0,0 @@
-
-// Generated from ratio.g4 by ANTLR 4.7
-
-#include "ratioVisitor.h"
-
-using namespace lucy;
