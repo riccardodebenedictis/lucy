4e269c9fc749ae3f722f69259faa111d5361f0f2
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-09-07 11:46:23 +0200

    minor cleanings..

--- smt-lib/theory.h
+++ smt-lib/theory.h
@@ -30,17 +30,18 @@ private:
   * Asks the theory to perform propagation after the given literal has been assigned. Returns true if the propagation succeeds or false if an inconsistency is found. In case of inconsistency, the confl vector is filled with the conflicting constraint.
   *
   * @param p the literal that has been assigned.
-  * @param confl the vector of literals representing the conflicting constraint.
+  * @param cnfl the vector of literals representing the conflicting constraint.
   * @return true if propagation succeeds or false if an inconsistency is found.
   */
-  virtual bool propagate(const lit &p, std::vector<lit> &confl) = 0;
+  virtual bool propagate(const lit &p, std::vector<lit> &cnfl) = 0;
 
   /**
   * Checks whether the theory is consistent with the given propositional assignments. Returns true if the theory is consistent or false if an inconsistency is found. In case of inconsistency, the confl vector is filled with the conflicting constraint.
   *
+  * @param cnfl the vector of literals representing the conflicting constraint.
   * @return true if the theory is consistent or false if an inconsistency is found.
   */
-  virtual bool check(std::vector<lit> &confl) = 0;
+  virtual bool check(std::vector<lit> &cnfl) = 0;
 
   /**
   * Notifies the theory that some information for subsequent backtracking might need to be stored.
