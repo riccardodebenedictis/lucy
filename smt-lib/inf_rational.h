30903374b42b9af71883e9ba1d5cb3bd390e6b0d
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-11-06 11:59:15 +0100

    solved minor issue with infinitesimals..

--- smt-lib/inf_rational.h
+++ smt-lib/inf_rational.h
@@ -4,73 +4,120 @@
 namespace smt
 {
 
-typedef long I;
-
-class inf_rational
+class inf_rational : public rational
 {
-  public:
-    inf_rational() : rat(0), inf(0) {}
-    inf_rational(rational f) : rat(f), inf(0) {}
-    inf_rational(I n, I d) : rat(rational(n, d)), inf(0) {}
-    inf_rational(rational f, I inf) : rat(f), inf(inf) {}
+public:
+  inf_rational() : inf(0) {}
+  inf_rational(rational rat) : rational(rat), inf(0) {}
+  inf_rational(I nun, I den) : rational(nun, den), inf(0) {}
+  inf_rational(rational rat, I inf) : rational(rat), inf(inf) {}
+  inf_rational(rational rat, rational inf) : rational(rat), inf(inf) {}
 
-  public:
-    rational get_rational() const { return rat; }
-    rational get_infinitesimal() const { return inf; }
+public:
+  rational infinitesimal() const { return inf; }
 
-    bool operator!=(const inf_rational &rhs) const;
-    bool operator<(const inf_rational &rhs) const;
-    bool operator<=(const inf_rational &rhs) const;
-    bool operator==(const inf_rational &rhs) const;
-    bool operator>=(const inf_rational &rhs) const;
-    bool operator>(const inf_rational &rhs) const;
+  bool operator!=(const inf_rational &rhs) const { return rational::operator!=(rhs) && inf != rhs.inf; };
+  bool operator<(const inf_rational &rhs) const { return rational::operator<(rhs) || (rational::operator==(rhs) && inf < rhs.inf); };
+  bool operator<=(const inf_rational &rhs) const { return rational::operator<=(rhs) || (rational::operator==(rhs) && inf <= rhs.inf); };
+  bool operator==(const inf_rational &rhs) const { return rational::operator==(rhs) && inf == rhs.inf; };
+  bool operator>=(const inf_rational &rhs) const { return rational::operator>=(rhs) || (rational::operator==(rhs) && inf >= rhs.inf); };
+  bool operator>(const inf_rational &rhs) const { return rational::operator>(rhs) || (rational::operator==(rhs) && inf > rhs.inf); };
 
-    bool operator!=(const rational &rhs) const;
-    bool operator<(const rational &rhs) const;
-    bool operator<=(const rational &rhs) const;
-    bool operator==(const rational &rhs) const;
-    bool operator>=(const rational &rhs) const;
-    bool operator>(const rational &rhs) const;
+  bool operator!=(const rational &rhs) const override { return rational::operator!=(rhs) || inf.numerator() != 0; };
+  bool operator<(const rational &rhs) const override { return rational::operator<(rhs) || (rational::operator==(rhs) && inf.numerator() < 0); };
+  bool operator<=(const rational &rhs) const override { return rational::operator<=(rhs) || (rational::operator==(rhs) && inf.numerator() <= 0); };
+  bool operator==(const rational &rhs) const override { return rational::operator==(rhs) && inf.numerator() == 0; };
+  bool operator>=(const rational &rhs) const override { return rational::operator>=(rhs) || (rational::operator==(rhs) && inf.numerator() >= 0); };
+  bool operator>(const rational &rhs) const override { return rational::operator>(rhs) || (rational::operator==(rhs) && inf.numerator() > 0); };
 
-    bool operator!=(const I &rhs) const;
-    bool operator<(const I &rhs) const;
-    bool operator<=(const I &rhs) const;
-    bool operator==(const I &rhs) const;
-    bool operator>=(const I &rhs) const;
-    bool operator>(const I &rhs) const;
+  bool operator!=(const I &rhs) const override { return rational::operator!=(rhs) || inf.numerator() != 0; };
+  bool operator<(const I &rhs) const override { return rational::operator<(rhs) || (rational::operator==(rhs) && inf.numerator() < 0); };
+  bool operator<=(const I &rhs) const override { return rational::operator<=(rhs) || (rational::operator==(rhs) && inf.numerator() <= 0); };
+  bool operator==(const I &rhs) const override { return rational::operator==(rhs) && inf.numerator() == 0; };
+  bool operator>=(const I &rhs) const override { return rational::operator>=(rhs) || (rational::operator==(rhs) && inf.numerator() >= 0); };
+  bool operator>(const I &rhs) const override { return rational::operator>(rhs) || (rational::operator==(rhs) && inf.numerator() > 0); };
 
-    rational operator+(const inf_rational &rhs) const;
-    rational operator-(const inf_rational &rhs) const;
-    rational operator*(const inf_rational &rhs) const;
-    rational operator/(const inf_rational &rhs) const;
+  rational operator+(const inf_rational &rhs) const
+  {
+    rational c_inf = inf + rhs.inf;
+    return c_inf == 0 ? rational::operator+(rhs) : inf_rational(rational::operator+(rhs), c_inf);
+  };
+  rational operator-(const inf_rational &rhs) const
+  {
+    rational c_inf = inf - rhs.inf;
+    return c_inf == 0 ? rational::operator-(rhs) : inf_rational(rational::operator-(rhs), c_inf);
+  };
 
-    rational operator+(const rational &rhs) const;
-    rational operator-(const rational &rhs) const;
-    rational operator*(const rational &rhs) const;
-    rational operator/(const rational &rhs) const;
+  rational operator+(const rational &rhs) const override { return inf_rational(rational::operator+(rhs), inf); };
+  rational operator-(const rational &rhs) const override { return inf_rational(rational::operator-(rhs), inf); };
+  rational operator*(const rational &rhs) const override
+  {
+    rational c_inf = inf * rhs;
+    return c_inf == 0 ? rational::operator*(rhs) : inf_rational(rational::operator*(rhs), c_inf);
+  };
+  rational operator/(const rational &rhs) const override
+  {
+    rational c_inf = inf / rhs;
+    return c_inf == 0 ? rational::operator/(rhs) : inf_rational(rational::operator/(rhs), c_inf);
+  };
 
-    rational operator+(const I &rhs) const;
-    rational operator-(const I &rhs) const;
-    rational operator*(const I &rhs) const;
-    rational operator/(const I &rhs) const;
+  rational operator+(const I &rhs) const override { return inf_rational(rational::operator+(rhs), inf); };
+  rational operator-(const I &rhs) const override { return inf_rational(rational::operator-(rhs), inf); };
+  rational operator*(const I &rhs) const override
+  {
+    rational c_inf = inf * rhs;
+    return c_inf == 0 ? rational::operator*(rhs) : inf_rational(rational::operator*(rhs), c_inf);
+  };
+  rational operator/(const I &rhs) const override
+  {
+    rational c_inf = inf / rhs;
+    return c_inf == 0 ? rational::operator/(rhs) : inf_rational(rational::operator/(rhs), c_inf);
+  };
 
-    rational &operator+=(const inf_rational &rhs);
-    rational &operator-=(const inf_rational &rhs);
-    rational &operator*=(const inf_rational &rhs);
-    rational &operator/=(const inf_rational &rhs);
+  rational &operator+=(const inf_rational &rhs)
+  {
+    rational::operator+=(rhs);
+    inf += rhs.inf;
+    return *this;
+  }
+  rational &operator-=(const inf_rational &rhs)
+  {
+    rational::operator-=(rhs);
+    inf -= rhs.inf;
+    return *this;
+  }
 
-    rational &operator+=(const rational &rhs);
-    rational &operator-=(const rational &rhs);
-    rational &operator*=(const rational &rhs);
-    rational &operator/=(const rational &rhs);
+  rational &operator+=(const rational &rhs) override { return rational::operator+=(rhs); }
+  rational &operator-=(const rational &rhs) override { return rational::operator-=(rhs); }
+  rational &operator*=(const rational &rhs) override
+  {
+    rational::operator*=(rhs);
+    inf *= rhs;
+    return *this;
+  }
+  rational &operator/=(const rational &rhs) override
+  {
+    rational::operator/=(rhs);
+    inf /= rhs;
+    return *this;
+  }
 
-    rational &operator+=(const I &rhs);
-    rational &operator-=(const I &rhs);
-    rational &operator*=(const I &rhs);
-    rational &operator/=(const I &rhs);
+  rational &operator+=(const I &rhs) override { return rational::operator+=(rhs); }
+  rational &operator-=(const I &rhs) override { return rational::operator-=(rhs); }
+  rational &operator*=(const I &rhs) override
+  {
+    rational::operator*=(rhs);
+    inf *= rhs;
+    return *this;
+  }
+  rational &operator/=(const I &rhs) override
+  {
+    rational::operator/=(rhs);
+    inf /= rhs;
+    return *this;
+  }
 
-  private:
-    rational rat;
-    rational inf;
+private:
+  rational inf; // the infinitesimal part..
 };
 }
